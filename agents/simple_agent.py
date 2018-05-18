43362531fcac73fcb6f58c385824d6ef8e04d55c
samcheng <samcheng@ucdavis.edu>
2018-05-17 19:54:18 -0700

    finished the scripted agent on simple64, add tips to gtpysc2, upload replay for the simple_agent

--- agents/simple_agent.py
+++ agents/simple_agent.py
@@ -12,11 +12,15 @@ from pysc2.lib import features
 
 import time
 
-## function/action id
+## action id
 _BUILD_SUPPLYDEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
 _BUILD_BARRACKS = actions.FUNCTIONS.Build_Barracks_screen.id
 _NOOP = actions.FUNCTIONS.no_op.id
 _SELECT_POINT = actions.FUNCTIONS.select_point.id
+_TRAIN_MARINE = actions.FUNCTIONS.Train_Marine_quick.id
+_RALLY_UNITS_MINIMAP = actions.FUNCTIONS.Rally_Units_minimap.id # rally = group them together
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_ATTACK_MINIMAP = actions.FUNCTIONS.Attack_minimap.id
 
 ## get features id
 _PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
@@ -25,11 +29,14 @@ _UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
 ## unit ids
 _TERRAN_COMMANDCENTER = 18
 _TERRAN_SCV = 45
+_TERRAN_BARRACKS = 21
 
 ## parameters
 _PLAYER_SELF = 1 # ???
 _NOT_QUEUED = [0]
 _QUEUED = [1]
+_SUPPY_USED = 3
+_SUPPLY_MAX = 4
 
 
 class SimpleAgent(base_agent.BaseAgent):
@@ -40,11 +47,17 @@ class SimpleAgent(base_agent.BaseAgent):
     supply_depot_built = False
     barrack_built = False
 
+    # attacked and control
+    barracks_rallied = False
+    barracks_selected = False
+    army_selected = False
+    army_rallied = False
+
+
     def step(self, obs):
         """a agent that just mine very quickly
         and got defeated on full map"""
         super(SimpleAgent, self).step(obs)
-        time.sleep(0.5)
 
         # locate base
         if self.base_top_left is None:
@@ -79,7 +92,7 @@ class SimpleAgent(base_agent.BaseAgent):
                 self.supply_depot_built = True
                 return actions.FunctionCall(_BUILD_SUPPLYDEPOT, [_NOT_QUEUED, target])
 
-        # build barrack
+        # if build barrack avaiable, select a location and build
         elif not self.barrack_built:
             if _BUILD_BARRACKS in obs.observation["available_actions"]:
                 unit_type = obs.observation["screen"][_UNIT_TYPE]
@@ -88,6 +101,46 @@ class SimpleAgent(base_agent.BaseAgent):
                 target  = self.transformLocation(int(unit_x.mean()), 20, int(unit_y.mean()), 0)
                 self.barrack_built = True
                 return actions.FunctionCall(_BUILD_BARRACKS, [_NOT_QUEUED, target])
+
+        # control barrack
+        elif not self.barracks_rallied:
+            if not self.barracks_selected:
+                unit_type = obs.observation["screen"][_UNIT_TYPE]
+                unit_y, unit_x = (unit_type == _TERRAN_BARRACKS).nonzero()
+
+                # if there is a barrack, pick a location in screen
+                if unit_y.any():
+                    target = [int(unit_x.mean()), int(unit_y.mean())]
+                    self.barracks_selected = True
+                    return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
+
+            # group the units together
+            else:
+                self.barracks_rallied = True
+                if self.base_top_left:
+                    # the rally location is hard code for simplicity
+                    return actions.FunctionCall(_RALLY_UNITS_MINIMAP, [_NOT_QUEUED, [29, 21]])
+                return actions.FunctionCall(_RALLY_UNITS_MINIMAP, [_NOT_QUEUED, [29, 46]])
+
+        elif obs.observation["player"][_SUPPY_USED] < obs.observation["player"][_SUPPLY_MAX] and \
+                _TRAIN_MARINE in obs.observation["available_actions"]:
+            return actions.FunctionCall(_TRAIN_MARINE, [_NOT_QUEUED])
+
+        elif not self.army_rallied:
+            if not self.army_selected:
+                if _SELECT_ARMY in obs.observation["available_actions"]:
+                    self.army_selected = True
+                    self.barracks_selected = False
+
+                    return actions.FunctionCall(_SELECT_ARMY, [_NOT_QUEUED])
+                elif _ATTACK_MINIMAP in obs.observation["available_actions"]:
+                    self.army_rallied = True
+                    self.army_selected = False
+
+                    if self.base_top_left:
+                        return actions.FunctionCall(_ATTACK_MINIMAP, [_NOT_QUEUED, [39, 45]])
+                    return actions.FunctionCall(_ATTACK_MINIMAP, [_NOT_QUEUED, [21, 24]])
+
 
         return actions.FunctionCall(_NOOP, [])
 
