0a4a47a3da64c5335a25ee663423bb2d6fc2fe26
Sam Cheng <33079357+sampalomad@users.noreply.github.com>
2018-06-04 14:04:56 -0700

    build model update

--- agents/dueling_agent/dueling_agent.py
+++ agents/dueling_agent/dueling_agent.py
@@ -17,7 +17,7 @@ from pysc2.agents import base_agent
 from pysc2.lib import actions
 from pysc2.lib import features
 
-from .utils import preprocess_screen, screen_channel
+from utils import preprocess_screen, screen_channel
 
 _PLAYER_RELATIVE = features.PlayerRelative.ALLY
 
@@ -118,6 +118,7 @@ class DuelingAgent(object):
             strides=[1, 1],
             padding='same',
             activation=tf.nn.relu,
+            bias_initializer=tf.constant_initializer(0.1),
             name='sconv1'   # 1st conv2d feature layer
         )
 
@@ -130,11 +131,10 @@ class DuelingAgent(object):
             strides=[1, 1],
             padding='same',
             activation=tf.nn.relu,
+            bias_initializer=tf.constant_initializer(0.1),
             name='sconv2')  # 2nd conv2d feature layer
 
         # Compute spatial actions
-        # feat_conv = tf.concat([sconv2], axis=3) # concat minimap and screen on channel dimension
-
         spatial_action = tf.layers.conv2d(
             inputs=sconv2,
             filters=1,
@@ -142,6 +142,7 @@ class DuelingAgent(object):
             strides=(1, 1),
             padding='same',
             activation=None,
+            bias_initializer=tf.constant_initializer(0.1),
             name='saptial_action')
         spatial_action = tf.nn.softmax(tf.layers.flatten(spatial_action))
 
@@ -150,6 +151,7 @@ class DuelingAgent(object):
             inputs=tf.layers.flatten(inputs=self.info),
             units=256,
             activation=tf.tanh,
+            bias_initializer=tf.constant_initializer(0.1),
             name='info_fc')
 
         feat_fc = tf.concat([layers.flatten(sconv2), info_fc], axis=1)
@@ -157,19 +159,37 @@ class DuelingAgent(object):
             inputs=feat_fc,
             units=256,
             activation=tf.nn.relu,
+            bias_initializer=tf.constant_initializer(0.1),
             name='feat_fc')
         non_spatial_action = tf.layers.dense(inputs=feat_fc,
                                              units=len(actions.FUNCTIONS),
                                              activation=tf.nn.softmax,
                                              name='non_spatial_action')
 
-        q = tf.reshape(
-            tensor=tf.layers.dense(
+        # compute q value using dueling net
+        with tf.variable_scope('Value'):
+            self.V = tf.layers.dense(
                 inputs=feat_fc,
                 units=1,
                 activation=None,
-                name='q'),
-            shape=[-1])
+                bias_initializer=tf.constant_initializer(0.1),
+                name='V')
+        with tf.variable_scope('Advantage'):
+            self.A = tf.layers.dense(
+                inputs=feat_fc,
+                units=len(actions.FUNCTIONS),
+                activation=None,
+                bias_initializer=tf.constant_initializer(0.1),
+                name='A')
+        with tf.variable_scope('Q'):
+            q = self.V + (self.A - tf.reduce_mean(self.A, axis=1, keep_dims=True))
+        # q = tf.reshape(
+        #     tensor=tf.layers.dense(
+        #         inputs=feat_fc,
+        #         units=1,
+        #         activation=None,
+        #         name='q'),
+        #     shape=[-1]) # a shape of [-1] flattens into 1-D
 
         return spatial_action, non_spatial_action, q
 
@@ -188,12 +208,44 @@ class DuelingAgent(object):
             # c_name = ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
             self.spatial_action, self.non_spatial_action, self.q_eval = self.build_network()
 
+        # target value
+        # self.valid_spatial_action = tf.placeholder(tf.float32, [None], name='valid_spatial_action')
+        # self.spatial_action_selected = tf.placeholder(tf.float32, [None, self.ssize ** 2], name='spatial_action_selected')
+        # self.valid_non_spatial_action = tf.placeholder(tf.float32, [None, len(actions.FUNCTIONS)],name= 'valid_non_spatial_action')
+        # self.non_spatial_action_selected = tf.placeholder(tf.float32, [None, len(actions.FUNCTIONS)], name='non_spatial_action_selected')
+        self.q_target = tf.placeholder(tf.float32, [None, len(actions.FUNCTIONS)], name='q_target')
+
+        # # action log probability
+        # spatial_action_prob = tf.reduce_sum(self.spatial_action * self.spatial_action_selected, axis=1)
+        # spatial_action_log_prob = tf.log(tf.clip_by_value(spatial_action_prob, 1e-10, 1.))
+        #
+        # non_spatial_action_prob = tf.reduce_sum(self.non_spatial_action * self.non_spatial_action_selected, axis=1)
+        # valid_non_spatial_action_prob = tf.reduce_sum(self.non_spatial_action * self.valid_non_spatial_action, axis=1)
+        # valid_non_spatial_action_prob = tf.clip_by_value(valid_non_spatial_action_prob, 1e-10, 1.)
+        # non_spatial_action_prob = non_spatial_action_prob / valid_non_spatial_action_prob
+        # non_spatial_action_log_prob = tf.log(tf.clip_by_value(non_spatial_action_prob, 1e-10, 1.))
+        #
+        # # compute loss
+        # action_log_prob = self.valid_spatial_action * spatial_action_log_prob + non_spatial_action_log_prob
+        # advantage = tf.stop_gradient(self.q_target - self.q_eval)
+        # policy_loss = - tf.reduce_mean(action_log_prob * advantage)
+        # value_loss = - tf.reduce_mean(self.q_eval * advantage)
+
+        with tf.variable_scope('loss'):
+            self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
+        with tf.variable_scope('train'):
+            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+
         # ---------------------------target net for spatial, non-spatial---------------------------
+
+        with tf.variable_scope('target_net'):
+            _, _, self.q_next = self.build_network()
 
         pass
 
     def reset(self):
         """reward discount reset"""
+
         self.episodes += 1
 
     def step(self, obs):
@@ -205,7 +257,24 @@ class DuelingAgent(object):
         # obs.observation.screen_feature is (17, 64, 64)
         screen = np.array(obs.observation.feature_screen, dtype=np.float32)
         screen_input = np.expand_dims(preprocess_screen(screen), axis=0) # return (bs=1, channel=42, h=64, w=64)
-        # print("input shape: ", screen_input.shape)
+
+        # get available actions
+        info = np.zeros([1, self.isize], dtype=np.float32)
+        info[0, obs.observation['available_actions']] = 1
+
+        # run session to obtain spatial action output and non spatial action array
+        non_spatial_action, spatial_action = self.sess.run(
+            [self.non_spatial_action, self.spatial_action],
+            feed_dict={self.screen: screen_input, self.info: info})
+
+        # select action and spatial target
+        non_spatial_action = non_spatial_action.ravel() # flatten
+        spatial_action = spatial_action.ravel() # flatten
+        valid_actions = obs.observation['available_actions']
+        act_id = valid_actions[np.argmax(non_spatial_action[valid_actions])]
+        target = np.argmax(spatial_action)
+        target = [int(target // self.ssize), int(target % self.ssize)]
+
         self.steps += 1
         self.reward += obs.reward
         return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
@@ -219,26 +288,25 @@ class DuelingAgent(object):
         """when certain number of replay size reach, learn from minibatch replay"""
         pass
 
-# if __name__ == '__main__':
-#     agent = DuelingAgent()
-#     agent.setup(
-#         obs_spec=1,
-#         action_spec=1,
-#         screen_size=64,
-#         learning_rate=0.001,
-#         reward_decay=0.9,
-#         e_greedy=0.9,
-#         replace_target_iter=200,
-#         memory_size=2000,
-#         batch_size=32,
-#         e_greedy_increment=None,
-#         output_graph=False,
-#         sess=None
-#     )
-#
-#     # print([n.name for n in tf.get_default_graph().as_graph_def().node])
-#     agent.sess.run(agent.replace_target_op)
-#     print(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope='target_net'))
+
+if __name__ == '__main__':
+    agent = DuelingAgent()
+    agent.setup(
+        obs_spec=1,
+        action_spec=1,
+        screen_size=64,
+        learning_rate=0.001,
+        reward_decay=0.9,
+        e_greedy=0.9,
+        replace_target_iter=200,
+        memory_size=2000,
+        batch_size=32,
+        e_greedy_increment=None,
+        output_graph=False,
+        sess=None
+    )
+
+
 
 
 
